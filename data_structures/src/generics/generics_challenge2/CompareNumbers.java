c7facace81309d945b152cf1f592e7ec0b4ea05b
Juanes Saavedra <juanessaavedra777@gmail.com>
2024-10-10 16:52:38 -0500

    Finish challenge 2 generics

--- data_structures/src/generics/generics_challenge2/CompareNumbers.java
+++ data_structures/src/generics/generics_challenge2/CompareNumbers.java
@@ -18,4 +18,21 @@ public class CompareNumbers {
 
         return min;
     }
+
+    public static <T extends Comparable<T>> T maximum (T a, T b, T c, T d, T e) {
+        T max = a;
+        if (b.compareTo(max) > 0)
+            max = b;
+
+        if (c.compareTo(max) > 0)
+            max = c;
+
+        if (d.compareTo(max) > 0)
+            max = d;
+
+        if (e.compareTo(max) > 0)
+            max = e;
+
+        return max;
+    }
 }
--- data_structures/src/generics/generics_challenge2/Main.java
+++ data_structures/src/generics/generics_challenge2/Main.java
@@ -1,8 +1,55 @@
 package generics.generics_challenge2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        CompareNumbers<Integer> compareNumbers = new CompareNumbers<>();
-        System.out.println(compareNumbers.minimum(1, 2, 3, 4, 5));
+
+        Scanner scanner = new Scanner(System.in);
+        String option;
+        System.out.println("Welcome! What would you like to do?\n 1. Compare numbers (maximum)\n 2. Compare numbers (minimum)\n 3. Exit");
+        option = scanner.next();
+
+        switch (option) {
+            case "1":
+                System.out.println("Enter five numbers to find the maximum:");
+                System.out.print("Number 1: ");
+                int num1Max = scanner.nextInt();
+                System.out.print("Number 2: ");
+                int num2Max = scanner.nextInt();
+                System.out.print("Number 3: ");
+                int num3Max = scanner.nextInt();
+                System.out.print("Number 4: ");
+                int num4Max = scanner.nextInt();
+                System.out.print("Number 5: ");
+                int num5Max = scanner.nextInt();
+
+                System.out.println("The maximum value is: " + CompareNumbers.maximum(num1Max, num2Max, num3Max, num4Max, num5Max));
+                break;
+
+            case "2":
+                System.out.println("Enter five numbers to find the minimum:");
+                System.out.print("Number 1: ");
+                int num1Min = scanner.nextInt();
+                System.out.print("Number 2: ");
+                int num2Min = scanner.nextInt();
+                System.out.print("Number 3: ");
+                int num3Min = scanner.nextInt();
+                System.out.print("Number 4: ");
+                int num4Min = scanner.nextInt();
+                System.out.print("Number 5: ");
+                int num5Min = scanner.nextInt();
+
+                System.out.println("The minimum value is: " + CompareNumbers.minimum(num1Min, num2Min, num3Min, num4Min, num5Min));
+                break;
+
+            case "3":
+                System.out.println("Goodbye!");
+                break;
+
+            default:
+                System.out.println("Invalid option. Please choose a valid option.");
+                break;
+        }
     }
 }

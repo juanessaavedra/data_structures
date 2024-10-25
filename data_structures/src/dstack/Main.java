f00bfb96637ccac173940fa588e9d85c68104752
Juanes Saavedra <juanessaavedra777@gmail.com>
2024-10-25 18:07:39 -0500

    add stack book's excercise

---
+++ data_structures/src/dstack/Main.java
@@ -0,0 +1,49 @@
+package dstack;
+
+import tablas_hash.Book;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/*
+
+*  */
+public class Main {
+    public static void main(String[] args) {
+        Stack<Libro> pilaLibros = new Stack<>();
+        String option = "1";
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Bienvenido ¿Qué desea hacer?");
+            System.out.println(("1. Agregar libro a la pila \n 2. Eliminar libro de la pila \n 3. Ver pila de libros  \n 4. Ver el ultimo elemento de la pila  \n 5. Exit"));
+            option = scanner.next();
+
+            switch (option) {
+                case "1":
+                    System.out.println("Agregar libro");
+                    System.out.println("Ingrese el nombre del libro:");
+                    String nombre = scanner.next();
+                    System.out.println("Ingrese el autor:");
+                    String autor = scanner.next();
+                    System.out.println("Libro agregado a la pila");
+                    Libro libro = new Libro(nombre, autor);
+                    pilaLibros.push(libro);
+                    break;
+                case "2":
+                    String libroEliminado = pilaLibros.pop().getNombre();
+                    System.out.println("Libro: " + libroEliminado + " eliminado de la pila");
+                    break;
+                case "3":
+                    System.out.println("Pila de libros:" + pilaLibros);
+                    break;
+                case "4":
+                    System.out.println("Ultimo libro de la pila: " + pilaLibros.peek());
+                    break;
+
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        } while(!option.equals("5"));
+    }
+}
---
+++ data_structures/src/dstack/STACK.md
@@ -0,0 +1,16 @@
+# Stack
+- LIFO data structure (Last In First Out)
+- Store objects into a sort of "vertical tower"
+- push() - add an element to the top of the stack
+- pop() - remove an element from the top of the stack
+
+- Iterar sobre los elementos de la pila es menos eficiente respecto a otras estructuras de datos, ya que la pila no esta diseñada para ser recorrida.
+- Buscar un elemento especifico requiere vaciar la pila o usar una estructura auxiliar, siendo ineficiente'
+- Se puede acceder facilmente al ultimo elemento agregado (tope de la pila) a traves de peek(), pero no se puede acceder al primer elemento de la pila sin vaciarla.
+
+## Usos de Stack
+- Undo/Redo features in applications
+- Moving back/forward through browser history
+- Backtracking in algorithms
+- Calling functions (call stack)
+

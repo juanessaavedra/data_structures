66bab4d8bc46a510bc31f541aecb21d134cb0c1a
Juanes Saavedra <juanessaavedra777@gmail.com>
2024-10-27 18:49:15 -0500

    Update stack

--- data_structures/src/dstack/Main.java
+++ data_structures/src/dstack/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /*
-
+    * Programa que simula una pila de libros
 *  */
 public class Main {
     public static void main(String[] args) {
@@ -14,12 +14,12 @@ public class Main {
         String option = "1";
         do {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Bienvenido ¿Qué desea hacer?");
+            System.out.println("Bienvenido ¿Qué desea hacer?"); //Menu
             System.out.println(("1. Agregar libro a la pila \n 2. Eliminar libro de la pila \n 3. Ver pila de libros  \n 4. Ver el ultimo elemento de la pila  \n 5. Exit"));
             option = scanner.next();
 
             switch (option) {
-                case "1":
+                case "1": //Agregar libro
                     System.out.println("Agregar libro");
                     System.out.println("Ingrese el nombre del libro:");
                     String nombre = scanner.next();
@@ -29,18 +29,18 @@ public class Main {
                     Libro libro = new Libro(nombre, autor);
                     pilaLibros.push(libro);
                     break;
-                case "2":
+                case "2": // Eliminar libro de la pila
                     String libroEliminado = pilaLibros.pop().getNombre();
                     System.out.println("Libro: " + libroEliminado + " eliminado de la pila");
                     break;
-                case "3":
+                case "3": //Ver pila de libros
                     System.out.println("Pila de libros:" + pilaLibros);
                     break;
-                case "4":
+                case "4": //Ver el ultimo elemento de la pila
                     System.out.println("Ultimo libro de la pila: " + pilaLibros.peek());
                     break;
 
-                default:
+                default: //Opcion invalida
                     System.out.println("Invalid option");
                     break;
             }
